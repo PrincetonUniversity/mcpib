83c2ab3be50ffde930ced8a17ede7c5e50813021
Jim Bosch <jbosch@astro.princeton.edu>
2014-11-16 16:49:31 -0500

    Add high-level Callable class to aggregate CallableOverloads.

---
+++ include/mcpib/Callable.hpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2014, Jim Bosch
+ * All rights reserved.
+ *
+ * mcpib is distributed under a simple BSD-like license;
+ * see the LICENSE file that should be present in the root
+ * of the source distribution.
+ */
+#ifndef MCPIB_Callable_hpp_INCLUDED
+#define MCPIB_Callable_hpp_INCLUDED
+
+#include "mcpib/CallableOverload.hpp"
+
+namespace mcpib {
+
+class Callable {
+public:
+
+    template <typename Result, typename ...Args>
+    void addOverload(
+        std::function<Result(Args...)> function,
+        std::initializer_list<std::string> names,
+        TypeRegistry & registry
+    ) {
+        _overloads.push_back(makeCallableOverload(function, names, registry));
+    }
+
+    PyPtr call(PyPtr const & args, PyPtr const & kwds) const;
+
+private:
+    std::vector<std::unique_ptr<CallableOverloadBase>> _overloads;
+};
+
+} // namespace mcpib
+
+#endif // !MCPIB_Callable_hpp_INCLUDED
---
+++ lib/src/Callable.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2014, Jim Bosch
+ * All rights reserved.
+ *
+ * mcpib is distributed under a simple BSD-like license;
+ * see the LICENSE file that should be present in the root
+ * of the source distribution.
+ */
+
+#include "mcpib/Callable.hpp"
+
+#include <limits>
+
+namespace mcpib {
+
+PyPtr Callable::call(PyPtr const & args, PyPtr const & kwds) const {
+    std::vector<CallableOverloadData> data;
+    data.reserve(_overloads.size());
+    std::size_t best_index = -1;
+    Penalty best_penalty = std::numeric_limits<unsigned int>::max();
+    for (std::size_t i = 0; i < _overloads.size(); ++i) {
+        data.emplace_back(args, kwds, _overloads[i].get());
+        if (data[i].getState() == CallableOverloadData::SUCCESS) {
+            Penalty new_penalty = data[i].getPenalty();
+            if (new_penalty < best_penalty) {
+                best_index = i;
+                best_penalty = new_penalty;
+                if (best_penalty == 0) break;
+            }
+        }
+    }
+    if (best_index < 0) {
+        // no matching overload; need to raise an exception
+    }
+    return data[best_index].call();
+}
+
+} // namespace mcpib
