86774d7a9ecdaf8204f0db983759a1c7a7c6dded
Jim Bosch <jbosch@astro.princeton.edu>
2015-03-05 18:37:32 -0500

    Rewrite test build scripts and start test for wrapper exceptions

---
+++ tests/exceptions_test.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2014, Jim Bosch
+# All rights reserved.
+#
+# mcpib is distributed under a simple BSD-like license;
+# see the LICENSE file that should be present in the root
+# of the source distribution.
+#
+
+import unittest
+import os
+import sys
+
+buildPythonPath = os.path.join(os.path.split(__file__)[0], "..", "python")
+if os.path.exists(buildPythonPath): sys.path.insert(0, buildPythonPath)
+
+import mcpib
+
+class ExceptionsTestCase(unittest.TestCase):
+
+
+    def testModule(self):
+        """Test that custom exceptions are defined within the mcpib module.
+        """
+        self.assertTrue(issubclass(mcpib.WrapperError, Exception))
+        self.assertTrue(issubclass(mcpib.UnknownCppException, mcpib.WrapperError))
+
+if __name__ == "__main__":
+    unittest.main()
